ee0f84a6cdfbb249b3b742dd67de41032b65b6bd
Lucas Garron <code@garron.net>
2025-10-07 22:38:45 -0700

    Initial version.

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY: lint
+lint:
+	bun x @biomejs/biome check
+
+.PHONY: format
+format:
+	bun x @biomejs/biome check --write
+
+.PHONY: publish
+publish:
+	npm publish
---
+++ src/index.ts
@@ -0,0 +1,139 @@
+import { cp, mkdir, readFile, stat, writeFile } from "node:fs/promises";
+import { homedir } from "node:os";
+import { basename, dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { default as trash } from "trash";
+import { xdgCache, xdgConfig, xdgData, xdgState } from "xdg-basedir";
+
+// TODO: classes for relative vs. absolute?
+
+export class Path {
+  // @ts-expect-error ts(2564): False positive. https://github.com/microsoft/TypeScript/issues/32194
+  #path: string;
+  constructor(path: string | URL | Path) {
+    if (path instanceof Path) {
+      this.#setNormalizedPath(path.#path);
+      return;
+    }
+    if (path instanceof URL) {
+      this.#setNormalizedPath(fileURLToPath(path));
+      return;
+    }
+    if (typeof path === "string") {
+      this.#setNormalizedPath(path);
+      return;
+    }
+    throw new Error("Invalid path");
+  }
+
+  #setNormalizedPath(path: string) {
+    this.#path = join(path);
+  }
+
+  toString(): string {
+    return this.#path;
+  }
+
+  /// Constructs a new path by appending the given path segments.
+  // TODO: accept `Path` inputs?
+  join(...segments: string[]): Path {
+    return new Path(join(this.#path, ...segments));
+  }
+
+  extendBasename(suffix: string): Path {
+    const joinedSuffix = join(suffix);
+    if (joinedSuffix !== basename(joinedSuffix)) {
+      throw new Error("Invalid suffix to extend file name.");
+    }
+    // TODO: join basename and dirname instead?
+    return new Path(this.#path + joinedSuffix);
+  }
+
+  get parent(): Path {
+    return new Path(dirname(this.#path));
+  }
+
+  get basename(): string {
+    return basename(this.#path);
+  }
+
+  async exists(constraints?: {
+    mustBe: "file" | "directory";
+  }): Promise<boolean> {
+    let stats: Awaited<ReturnType<typeof stat>>;
+    try {
+      stats = await stat(this.#path);
+      // biome-ignore lint/suspicious/noExplicitAny: TypeScript limitation
+    } catch (e: any) {
+      if (e.code === "ENOENT") {
+        return false;
+      }
+      throw e;
+    }
+    if (!constraints?.mustBe) {
+      return true;
+    }
+    switch (constraints?.mustBe) {
+      case "file": {
+        if (stats.isFile()) {
+          return true;
+        }
+        throw new Error(`Path exists but is not a file: ${this.#path}`);
+      }
+      case "directory": {
+        if (stats.isDirectory()) {
+          return true;
+        }
+        throw new Error(`Path exists but is not a directory: ${this.#path}`);
+      }
+      default: {
+        throw new Error("Invalid path type constraint");
+      }
+    }
+  }
+
+  async existsAsFile(): Promise<boolean> {
+    return this.exists({ mustBe: "file" });
+  }
+
+  async existsAsDir(): Promise<boolean> {
+    return this.exists({ mustBe: "directory" });
+  }
+
+  async mkdir(options: Parameters<typeof mkdir>[1]): Promise<void> {
+    await mkdir(this.#path, options);
+  }
+
+  // TODO: check idempotency semantics when the destination exists and is a folder.
+  async copy(destination: string | URL | Path): Promise<void> {
+    await cp(this.#path, new Path(destination).#path);
+  }
+
+  // TODO: check idempotency semantics when the destination exists and is a folder.
+  async trash(): Promise<void> {
+    await trash(this.#path);
+  }
+
+  async fileText(): Promise<string> {
+    return readFile(this.#path, "utf-8");
+  }
+
+  async fileJSON<T>(): Promise<T> {
+    return JSON.parse(await this.fileText());
+  }
+
+  async write(s: string): Promise<void> {
+    await writeFile(this.#path, s);
+  }
+
+  static get homedir(): Path {
+    return new Path(homedir());
+  }
+
+  static xdg = {
+    cache: new Path(xdgCache ?? Path.homedir.join(".cache")),
+    config: new Path(xdgConfig ?? Path.homedir.join(".config")),
+    data: new Path(xdgData ?? Path.homedir.join(".local/share")),
+    state: new Path(xdgState ?? Path.homedir.join(".local/state")),
+  };
+}
